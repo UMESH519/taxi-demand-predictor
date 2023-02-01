6cb6ce8dc8b77fa9afe865d5d57f419bab88ccb9
UMESH519 <umesh.karamchandani@gmail.com>
2023-01-31 19:03:19 -0600

    Wrote inference.py and some other package updates

--- src/config.py
+++ src/config.py
@@ -9,3 +9,13 @@ load_dotenv(PARENT_DIR / '.env')
 HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
+
+
+FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
+FEATURE_GROUP_VERSION = 1
+FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
+FEATURE_VIEW_VERSION = 1
+MODEL_NAME = "taxi_demand_predictor_next_hour"
+MODEL_VERSION = 2
+
+N_FEATURES = 24 * 28
